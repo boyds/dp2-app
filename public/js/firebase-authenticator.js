2e9744afca3c196dc3da534b00fcf75ce34d68eb
Kyle Van Berendonck <kvanberendonck@gmail.com>
2017-04-01 17:23:34 +1100

    Add mithril for use as a SPA router

--- public/js/firebase-authenticator.js
+++ public/js/firebase-authenticator.js
@@ -2,6 +2,7 @@
 // Create anonymous authenticator provider
 firebase.auth().signInAnonymously().then(function(result) {
     $("#authentication-loader").remove();
+    // Fire the authentication complete event (handing control over to the SPA router)
     $.event.trigger({ type: "authentication_complete" });
 }).catch(function(error) {
     alert("There was a fatal authentication error. Check internet/service connectivity.");
