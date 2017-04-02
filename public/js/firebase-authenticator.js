6bdd9371ea52902c65a5730bfacb03c0565ac296
Kyle Van Berendonck <kvanberendonck@gmail.com>
2017-04-03 09:56:32 +1000

    Wibble

--- public/js/firebase-authenticator.js
+++ public/js/firebase-authenticator.js
@@ -6,5 +6,6 @@ firebase.auth().signInAnonymously().then(function(result) {
     // Fire the authentication complete event (handing control over to the SPA router)
     $.event.trigger({ type: "authentication_complete" });
 }).catch(function(error) {
+    // FIXME: If there is actually an authentication error, show a page error w/ a retry button rather than an alert()
     alert("There was a fatal authentication error. Check internet/service connectivity.");
 });
